6f73bbcdc80a2bcaacc7df2dd726f9bcc8f42757
Priyanshu Ahuja <85331994+Priyanshuahuja72@users.noreply.github.com>
2023-04-11 17:04:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+let formbutton = document.getElementById("btn");
+function promiseApi1(name , age)
+{
+ return new Promise((resolve , reject) => {
+    if(age > 18)
+    {
+        setTimeout(() => {
+            resolve(
+                alert(`welcome , ${name}, You can vote`)
+                // console.log(`You can vote , ${name}`)
+            )
+        } , 4000)
+    }
+    else
+    {
+        alert(`Oh sorry ${name}. You aren't old enough.`)
+    }
+ })
+}
+formbutton.addEventListener("click" , (event) => {
+    event.preventDefault();
+    let nameinput = document.getElementById("name").value;
+    let ageinput = document.getElementById("age").value;
+    if (nameinput == "" && ageinput == "") {
+        alert("It should not be empty")
+    }
+    else
+    {
+        promiseApi1(nameinput , ageinput);
+    }
+})
